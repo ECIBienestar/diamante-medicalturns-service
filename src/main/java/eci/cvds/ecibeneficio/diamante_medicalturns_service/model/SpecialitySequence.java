0b1b22a752d3b70d567463217114e44eb59aea0c
dadc1007 <dadica1007@gmail.com>
2025-05-03 00:37:18 -0500

    Implemented tuen service

---
+++ src/main/java/eci/cvds/ecibeneficio/diamante_medicalturns_service/model/SpecialitySequence.java
@@ -0,0 +1,18 @@
+package eci.cvds.ecibeneficio.diamante_medicalturns_service.model;
+
+import eci.cvds.ecibeneficio.diamante_medicalturns_service.utils.enums.SpecialityEnum;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "speciality_sequence")
+@Data
+@NoArgsConstructor
+public class SpecialitySequence {
+  @Id
+  @Enumerated(EnumType.STRING)
+  private SpecialityEnum speciality;
+
+  private String sequence;
+}
---
+++ src/main/java/eci/cvds/ecibeneficio/diamante_medicalturns_service/model/Turn.java
@@ -0,0 +1,40 @@
+package eci.cvds.ecibeneficio.diamante_medicalturns_service.model;
+
+import eci.cvds.ecibeneficio.diamante_medicalturns_service.utils.enums.PriorityEnum;
+import eci.cvds.ecibeneficio.diamante_medicalturns_service.utils.enums.SpecialityEnum;
+import eci.cvds.ecibeneficio.diamante_medicalturns_service.utils.enums.StatusEnum;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "turn")
+@Data
+@NoArgsConstructor
+public class Turn {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "doctor_id", referencedColumnName = "id")
+  private Doctor doctor;
+
+  private String code;
+  private SpecialityEnum speciality;
+  private PriorityEnum priority;
+  private LocalDateTime date;
+  private StatusEnum status = StatusEnum.PENDING;
+  private int levelAttention;
+
+  public Turn(SpecialityEnum speciality, PriorityEnum priority, LocalDateTime date) {
+    this.speciality = speciality;
+    this.priority = priority;
+    this.date = date;
+  }
+}
