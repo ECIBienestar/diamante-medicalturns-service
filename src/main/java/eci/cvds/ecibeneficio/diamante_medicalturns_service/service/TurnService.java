a5045149c1d4274eb6c1d631dda3fc8934a1da29
dadc1007 <dadica1007@gmail.com>
2025-05-04 13:09:44 -0500

    Implemented reports service

--- src/main/java/eci/cvds/ecibeneficio/diamante_medicalturns_service/service/TurnService.java
+++ src/main/java/eci/cvds/ecibeneficio/diamante_medicalturns_service/service/TurnService.java
@@ -3,8 +3,14 @@ package eci.cvds.ecibeneficio.diamante_medicalturns_service.service;
 import eci.cvds.ecibeneficio.diamante_medicalturns_service.dto.request.CreateTurnRequest;
 import eci.cvds.ecibeneficio.diamante_medicalturns_service.model.Doctor;
 import eci.cvds.ecibeneficio.diamante_medicalturns_service.model.Turn;
+import eci.cvds.ecibeneficio.diamante_medicalturns_service.repository.projection.AverageLevelByRole;
+import eci.cvds.ecibeneficio.diamante_medicalturns_service.repository.projection.AverageLevelBySpeciality;
+import eci.cvds.ecibeneficio.diamante_medicalturns_service.repository.projection.CountByRole;
+import eci.cvds.ecibeneficio.diamante_medicalturns_service.repository.projection.CountBySpeciality;
+import eci.cvds.ecibeneficio.diamante_medicalturns_service.utils.enums.RoleEnum;
 import eci.cvds.ecibeneficio.diamante_medicalturns_service.utils.enums.SpecialityEnum;
 import eci.cvds.ecibeneficio.diamante_medicalturns_service.utils.enums.StatusEnum;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,4 +32,16 @@ public interface TurnService {
   void updateLevelAttention(Long id, int levelAttention);
 
   void updateDoctor(Long turnId, Doctor doctor);
+
+  List<AverageLevelByRole> getAverageLevelAttentionByRole(
+      RoleEnum role, LocalDate start, LocalDate end);
+
+  List<CountByRole> getTurnCountByRole(
+      RoleEnum role, LocalDate start, LocalDate end, StatusEnum status);
+
+  List<AverageLevelBySpeciality> getAverageLevelAttentionBySpeciality(
+      SpecialityEnum speciality, LocalDate start, LocalDate end);
+
+  List<CountBySpeciality> getTurnCountBySpeciality(
+      SpecialityEnum speciality, LocalDate start, LocalDate end, StatusEnum status);
 }
