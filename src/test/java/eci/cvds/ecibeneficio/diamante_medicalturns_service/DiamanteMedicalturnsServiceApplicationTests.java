99767aede0b459575c60e4cbc610b738d05d605e
ChenteChaurio <vicentegarzonrios@gmail.com>
2025-05-05 22:08:08 -0500

    tercer intento del deploy

--- src/test/java/eci/cvds/ecibeneficio/diamante_medicalturns_service/DiamanteMedicalturnsServiceApplicationTests.java
+++ src/test/java/eci/cvds/ecibeneficio/diamante_medicalturns_service/DiamanteMedicalturnsServiceApplicationTests.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("dev")
-@SpringBootTest
-class DiamanteMedicalturnsServiceApplicationTests {
+//@ActiveProfiles("prod")
+//@SpringBootTest
+//class DiamanteMedicalturnsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
